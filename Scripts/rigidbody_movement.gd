d028c146504c851f34152692cf99b5d702c46f9c
Chuyoiva <39066961+Chuyoiva@users.noreply.github.com>
2020-08-20 16:59:29 -0500

    Fisicas de blob nuevas
    La imagen del poligono se relaciona correctamente con los vertices con colision
    Las colisiones se adaptan al radio del circulo y cantidad de vertices
    Le agregue un offset para que la textura este encima de las colisiones

--- Scripts/rigidbody_movement.gd
+++ Scripts/rigidbody_movement.gd
@@ -1,63 +1,78 @@
 extends RigidBody2D
 
-
 var vertexPref = preload("res://Prefabs/pref_vertex.tscn")
-var numberOfVertex = 20
-var innerSprings = []
+var polygonPref = preload("res://Prefabs/pref_PolygonTexture.tscn")
+var numberOfVertex = 50
+var polygonObj = null
 var vertices = []
 var radius = 100
+var moveForce = 75.0
+var vertexRadius = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Create_Circle()
-	Set_Springs()
-	pass
+	Configure_Joints()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	Input_button()
+func _physics_process(delta):
+	Connect_Vertex2Polygon()
+	Input_button(delta)
 	
-func Input_button():
-	if Input.is_action_pressed("left"):
-		add_force(Vector2(0,0), Vector2.LEFT)
-		
-	if Input.is_action_pressed("right"):
-		add_force(Vector2(0,0), Vector2.RIGHT)
+func Input_button(var delta):
+	if Input.is_action_pressed("ui_up"):
+		apply_impulse(Vector2.ZERO, Vector2.UP  * moveForce)
+	if Input.is_action_pressed("ui_left"):
+		apply_impulse(Vector2.ZERO, Vector2.LEFT  * moveForce)
+	if Input.is_action_pressed("ui_down"):
+		apply_impulse(Vector2.ZERO, Vector2.DOWN  * moveForce)
+	if Input.is_action_pressed("ui_right"):
+		apply_impulse(Vector2.ZERO, Vector2.RIGHT * moveForce)
+	
 
 func Create_Circle():
+	polygonObj = polygonPref.instance()
+	add_child(polygonObj)
+	var polyVertices = []
+
 	for i in range(numberOfVertex):
-		var spring = DampedSpringJoint2D.new()
-		spring.name = "spring " + str(i+1)
-		get_child(1).add_child(spring)
-		innerSprings.append(spring)
-		
+		#Calculates the circle radium
 		var angle = i * PI * 2 / numberOfVertex
 		var pos = Vector2(cos(angle), sin(angle))*radius
+		#Add the position into an Array
+		polyVertices.append(pos)
+		
+		#Instantiates a collision vertex 
 		var vertex = vertexPref.instance()
 		vertex.name = "vertex " + str(i+1)
-		add_child(vertex)
 		vertex.position = pos
 		vertices.append(vertex)
-		
-func Set_Springs():
-	for i in range(numberOfVertex):
-		innerSprings[i].set_node_a(get_node(".").get_path())
-		innerSprings[i].set_node_b(vertices[i].get_node(".").get_path())
-		innerSprings[i].set_exclude_nodes_from_collision(false)
-		innerSprings[i].set_length(120)
-		innerSprings[i].set_rest_length(120)
-		innerSprings[i].set_stiffness(1)
-		innerSprings[i].set_damping(16)
-		
-		var outerSpring = vertices[i].get_node("DampedSpringJoint2D")
-		outerSpring.set_node_a(vertices[i].get_node(".").get_path())
-		if i < numberOfVertex-2:
-			outerSpring.set_node_b(vertices[i+2].get_node(".").get_path())
-		else:
-			outerSpring.set_node_b(vertices[i-numberOfVertex+2].get_node(".").get_path())
-		
-		outerSpring.set_exclude_nodes_from_collision(false)
-		outerSpring.set_length(70)
-		outerSpring.set_rest_length(10)
-		outerSpring.set_stiffness(1)
-		outerSpring.set_damping(16)
+		get_node("Polygon2D").add_child(vertex)
+
+	#Creates a polygon using the Array's positions
+	polygonObj.set_polygon(polyVertices)
 	
+		
+func Configure_Joints():
+	vertexRadius = vertices[0].position.distance_to(vertices[1].position)/2
+	for vertex in vertices:
+		#Sets the vertex's Node A and B
+		var pinJoint = vertex.get_node("PinJoint2D")
+		pinJoint.set_node_a(pinJoint.get_parent().get_path())
+		pinJoint.set_node_b(get_node(".").get_path())
+		
+		#Sets vertex collision size
+		var circleShape = vertex.get_node("CollisionShape2D").get_shape()
+		circleShape.set_radius(vertexRadius);
+		
+	pass
+	
+func Connect_Vertex2Polygon():
+	var newPositions = []
+	for vertex in vertices:
+		var angle = get_node("Polygon2D").position.direction_to(vertex.position)
+		var distance = get_node("Polygon2D").position.distance_to(vertex.position) + vertexRadius
+		var offset = angle*distance
+		newPositions.append(offset)	
+		
+	polygonObj.set_polygon(newPositions)
